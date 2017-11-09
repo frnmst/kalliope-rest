8c1a0779f9ec94695c8a236340ca1921baa4aeaa
frnmst/Franco Masotti <franco.masotti@live.com>
2017-11-09 17:50:18 +0100

    Typo.

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     author='Franco Masotti',
     author_email='franco.masotti@student.unife.it',
     keywords='kalliope rest',
-    url='https://github.com/frnmst7kalliope-rest',
+    url='https://github.com/frnmst/kalliope-rest',
     python_requires='>=3',
     # This part was inspired by:
     # https://chriswarrick.com/blog/2014/09/15/python-apps-the-right-way-entry_points-and-scripts/
